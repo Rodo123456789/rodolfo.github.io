5512c606d1beb90723b754f2142c5ef2b2aaa328
Rodo123456789 <rodolfocruzibarra@gmail.com>
2025-11-16 12:58:07 -0600

    aaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rodolfo.github.io/",
-  base: "/", // ✔ correcto porque es un repositorio tipo "usuario.github.io"
   vite: {
     plugins: [tailwindcss()]
   }
